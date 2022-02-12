222520fb132aab6f7856e3fdbb25a5b7b203c282
YuruProGitHub <daigodevelopment@gmail.com>
2022-02-12 13:47:27 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/app">Home</router-link> |
+    <router-link to="/app/about">About</router-link>
   </div>
   <router-view />
 </template>
